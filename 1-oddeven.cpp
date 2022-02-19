65e1bf209458179a44a6fddbfbd2916e5ce52bc9
dhanashreepatil2801 <dhanashreepatil2801@gmail.com>
2022-02-19 17:23:59 +0530

    1-oddeven

---
+++ 1-oddeven.cpp
@@ -0,0 +1,17 @@
+//program to check if a number is odd or even
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Even"<<endl;
+    }else{
+        cout<<"Odd"<<endl;
+    }
+
+    return 0;
+}
