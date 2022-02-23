0f1c42fe021f7d494134e3998ad159422e964071
dhanashreepatil2801 <dhanashreepatil2801@gmail.com>
2022-02-23 19:45:21 +0530

    hollow diamond inscribed in a rectangle

---
+++ 3-advancepatternquestions/hollowdiamondinscribedinrect.cpp
@@ -0,0 +1,48 @@
+//*********
+//**** ****
+//***   ***
+//**     **
+//*       *
+//*       *
+//**     **
+//***   ***
+//**** ****
+//*********
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=n-i;j>0;j--){
+            cout<<"*";
+        }
+        for(int j=(2*i)-1;j>0;j--){
+            cout<<" ";
+        }
+        for(int j=n-i;j>0;j--){
+            if(j<5){
+                cout<<"*";
+            }            
+        }
+        cout<<endl;
+    }
+    for(int i=n-1; i>=0;i--){
+        for(int j=n-i;j>0;j--){
+            cout<<"*";
+        }
+        for(int j=(2*i)-1;j>0;j--){
+            cout<<" ";
+        }
+        for(int j=n-i;j>0;j--){
+            if(j<5){
+                cout<<"*";
+            } 
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
